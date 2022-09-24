1d4a7ecbbbc49fc63b61b068993214fd7a390690
Diegosticks <c.mekowulu@yahoo.com>
2022-09-24 03:58:40 +0100

    Add project files.

---
+++ DiegoBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DiegoBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiegoBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
